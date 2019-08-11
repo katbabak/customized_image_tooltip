6c261ec9c845e87079f57991d7ca6bd7b8bc5a0b
Kate Babak <kateryna@lanars.com>
2019-08-11 18:31:24 +0300

    add progress bar loader

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,8 +4,9 @@ import {
 } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatProgressBarModule } from '@angular/material';
 
 
 export const MATERIAL_MODULES = [
@@ -13,7 +14,8 @@ export const MATERIAL_MODULES = [
   MatIconModule,
   MatDialogModule,
   MatFormFieldModule,
-  MatSelectModule
+  MatSelectModule,
+  MatProgressBarModule
 ];
 
 @NgModule({
--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -14,6 +14,8 @@ export class ImageService {
 
   private imagesArray$: BehaviorSubject<Image[]> = new BehaviorSubject([]);
   imagesArrayObservable = this.imagesArray$.asObservable();
+  private loading: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  loadingObservable = this.loading.asObservable();
 
   constructor() {
 
@@ -21,17 +23,21 @@ export class ImageService {
 
   setNewImage(image: Image) {
     console.warn(localStorage.getItem(_IMAGES));
+    this.loading.next(true);
     const arrayFromLS: Image[] = localStorage.getItem(_IMAGES) ? JSON.parse(localStorage.getItem(_IMAGES)) : [];
     arrayFromLS.push(image);
     localStorage.setItem(_IMAGES, JSON.stringify(arrayFromLS));
     of(arrayFromLS).pipe(delay(2000)).subscribe((array) => {
+      this.loading.next(false);
       this.imagesArray$.next(array);
     });
   }
 
   getImages() {
+    this.loading.next(true);
     const arrayFromLS: Image[] = localStorage.getItem(_IMAGES) ? JSON.parse(localStorage.getItem(_IMAGES)) : [];
     of(arrayFromLS).pipe(delay(2000)).subscribe((array) => {
+      this.loading.next(false);
       this.imagesArray$.next(array);
     });
   }
